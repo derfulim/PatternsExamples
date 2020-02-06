49b27612d1d6b70a42f5388ee7cd7591c8f8f146
derfulim <barashenko@ukr.net>
2020-02-06 21:56:54 +0200

    added example for proxy pattern

---
+++ src/ua/training/proxy/ProxyApp.java
@@ -0,0 +1,49 @@
+package ua.training.proxy;
+
+public class ProxyApp {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("D://image.jpg");
+        image.display();
+
+    }
+}
+
+interface Image{
+    void display();
+}
+
+class RealImage implements Image{
+
+    String file;
+
+    public RealImage(String file) {
+        this.file = file;
+        load();
+    }
+
+    void load() {
+        System.out.println(file + " is loading...");
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Viewing of " + file);
+    }
+}
+
+class ProxyImage implements Image{
+    String file;
+    RealImage image;
+    public ProxyImage(String file) {
+        this.file = file;
+    }
+
+    @Override
+    public void display() {
+        if (image == null) {
+            image = new RealImage(file);
+        }
+        image.display();
+    }
+}
+
